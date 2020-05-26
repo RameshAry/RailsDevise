241a7869ff2b7ae4c79070edd6ce3d7bba1b6801
RameshHolly <rameshbaglung90@gmail.com>
2020-05-26 10:17:10 +0000

    Finish User Singup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcom to Devise App Made By Ramesh and Hanh"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+  #define user params
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
