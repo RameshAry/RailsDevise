fe93a9a8ad6d0ee43c24eedb6696a4833d749b44
RameshHolly <rameshbaglung90@gmail.com>
2020-05-28 13:14:31 +0000

    Add user microposts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,4 +28,5 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
+  
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @mircopost = current_user.microposts.build
+      @feed_item = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
