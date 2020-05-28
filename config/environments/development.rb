e6d0a393f56b31aee35cabf8a7a44e6c78052d3d
RameshHolly <rameshbaglung90@gmail.com>
2020-05-28 07:35:43 +0000

    Fix raise delivery error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   
   host = '<hex string>.vfs.cloud9.us-east-2.amazonaws.com'     # Cloud IDE
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
