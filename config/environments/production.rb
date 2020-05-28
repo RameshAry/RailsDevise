39de83a8c19d1242154004b88ef16f9dd2112e54
RameshHolly <rameshbaglung90@gmail.com>
2020-05-28 07:45:18 +0000

    change active storage documentation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,6 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
   
-   config.active_storage.service = :amazon
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
