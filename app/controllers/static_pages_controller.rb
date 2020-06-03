9960ea049ad70356786930e89d6d594f500fd2bc
RameshHolly <rameshbaglung90@gmail.com>
2020-06-03 03:26:04 +0000

    added merge feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,4 +15,14 @@ class StaticPagesController < ApplicationController
   
   def contact
   end
+  
+  def search
+    if params[:search].blank?  
+    redirect_to(root_path, alert: "Empty field!") and return  
+    else
+      @parameter = params[:search].downcase  
+      @results = User.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
+    end
+      
+  end
 end
